32ab644fb9e28a821f746f1b5e1fb9098af54efc
PikaPutri17 <putriamandapika@gmail.com>
2024-11-30 23:03:10 +0700

    Belajar While

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,13 @@ Route::get('profile', function () {
     // return  view('profile', compact('nama'));
     return view('profile')->with('nama', $nama);
 });
+
+Route::get('array', function () {
+    // Infinite Loop
+    // Ketika kita membuat perulangan kita harus menetapkan nilai falsenya kalau tidak maka akan terjadi Infinite Loop atau Looping Forever
+    $nilai_awal = 1;
+    while ($nilai_awal <= 100) {
+        echo 'Hello Laravel ' . $nilai_awal . 'x<br>';
+        $nilai_awal++;
+    }
+});
