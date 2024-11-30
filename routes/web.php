8fc925e06f6dea13fae40aa45e9c0074738cc653
PikaPutri17 <putriamandapika@gmail.com>
2024-11-30 23:16:29 +0700

    Belajar FOR

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,7 @@ Route::get('profile', function () {
 });
 
 Route::get('array', function () {
-    // Infinite Loop
-    // Ketika kita membuat perulangan kita harus menetapkan nilai falsenya kalau tidak maka akan terjadi Infinite Loop atau Looping Forever
-    $nilai_awal = 1;
-    while ($nilai_awal <= 100) {
-        echo 'Hello Laravel ' . $nilai_awal . 'x<br>';
-        $nilai_awal++;
+    for ($i = 1; $i <= 5; $i++) {
+        echo 'Hello Word ' . $i . 'x<br>';
     }
 });
