f9af087ccf2bb38daf0ec5a3be996642fd198c8b
PikaPutri17 <putriamandapika@gmail.com>
2024-12-08 23:55:05 +0700

    Membuat controller mahasiswaController menggunakan controller resource dengan membuat halaman mahasiswa dan create mahasiswa

--- resources/views/mahasiswa/create.blade.php
+++ resources/views/mahasiswa/create.blade.php
@@ -37,26 +37,10 @@
   </nav>
 
   <div class="container">
-    <h1>Ini adalah halaman Mahasiswa</h1>
+    <h1>Ini Adalah Halaman Tambah Mahasiswa</h1>
 
-    <div class="row">
-      <div class="col-sm-6">
-      <h4>Tabel Mahasiswa</h4>
-        <table class="table table-primary table-sm table-hover table-striped table-bordered text-center">
-          <thead>
-            <tr>
-              <th>NIM</th>
-              <th>Nama Mahasiswa</th>
-              <th>Jenis Kelamin</th>
-              <th colspan="2">TTL</th>
-            </tr>
-          </thead>
-          <tbody>
-          
-          </tbody>
-        </table>
-      </div> 
-      <div class="col-sm-6">
+    <div class="row">    
+      <div class="col-sm-12">
         <h4>Form Mahasiswa</h4>
         <form action="" method="GET">
           <div class="row">
@@ -85,9 +69,12 @@
          </div>
          
           <div class="row mt-2">
-           <div class="col-sm-12">
+           <div class="col-sm-6">
             <button class="btn btn-info" style="width: 100%" type="submit">Simpan</button>
            </div> 
+           <div class="col-sm-6">
+              <a href="/mahasiswa" class="btn btn-secondary" style="width: 100%">Kembali</a>
+           </div>
           </div> 
      
         </form>
