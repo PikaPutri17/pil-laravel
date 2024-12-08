bfb4700b5fbed9b41806bb586a4063467b22ed2f
PikaPutri17 <putriamandapika@gmail.com>
2024-12-08 22:36:39 +0700

    Membuat controller mahasiswaController

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -52,14 +52,7 @@
             </tr>
           </thead>
           <tbody>
-           @for ($i = 0; $i < $jumlah; $i++)
-                <tr>
-                  <td>{{$nim[$i]}}</td>
-                  <td>{{$nama[$i]}}</td>
-                  <td>Perempuan</td>
-                  <td>Kota Medan</td>
-                </tr>
-           @endfor
+          
           </tbody>
         </table>
       </div> 
